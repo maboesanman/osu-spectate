6ceac58b0800a105c4ccf321c0612e1d206b7a3f
Mason Boeman <maboesanman@gmail.com>
2016-01-19 22:54:56 -0600

    modified x and y computation to guarantee all circles are entirely onscreen

--- Osu!Spectate/Computation.cs
+++ Osu!Spectate/Computation.cs
@@ -12,9 +12,40 @@ namespace OsuSpectate
     {
         public static float XComputation(float x, float OriginX, float OriginY, float width, float height, int windowWidth, int windowHeight)
         {
-            if (windowWidth * width / windowHeight / height < 10.0f / 9.0f)
+            float xrelative;
+            float w = width * windowWidth;
+            float h = height * windowHeight;
+            if(w/h>19.0f/15.0f)
             {
-                return (x / 512.0f * width * windowWidth) / windowWidth + OriginX;
+                xrelative = ((x + 48.0f) / (608.0f) * (h * 19.0f / 15.0f) + ((w - h * 19.0f / 15.0f) / (2.0f))) / w;
+            }
+            else
+            {
+                xrelative = (x + 48.0f) / (608.0f);
+            }
+            return width * xrelative + OriginX;
+        }
+        public static float YComputation(float y, float OriginX, float OriginY, float width, float height, int windowWidth, int windowHeight)
+        {
+            float yrelative;
+            float w = width * windowWidth;
+            float h = height * windowHeight;
+            if (w / h > 19.0f / 15.0f)
+            {
+                yrelative = (384-y + 48.0f) / (480.0f);
+            }
+            else
+            {
+                yrelative = ((384-y + 48.0f) / (480.0f) * (w * 15.0f / 19.0f) + ((h - w * 15.0f / 19.0f) / (2.0f))) / h;
+            }
+            return height * yrelative + OriginY;
+        }
+        /*
+        public static float XComputation(float x, float OriginX, float OriginY, float width, float height, int windowWidth, int windowHeight)
+        {
+            if (windowWidth * width / windowHeight / height < 19.0f / 15.0f)
+            {
+                return ((x + 48) / 608.0f * width * windowWidth) / windowWidth + OriginX;
             }
             else
             {
@@ -32,6 +63,7 @@ namespace OsuSpectate
                 return (height * windowHeight / 2.0f - height * windowHeight * 5.0f / 12.0f + (1.0f - y / 384.0f) * height * windowHeight * 5.0f / 6.0f) / windowHeight + OriginY;
             }
         }
+        */
         public static float Distance(PointF p1, PointF p2)
         {
             return (float)Math.Sqrt((p1.X - p2.X) * (p1.X - p2.X) + (p1.Y - p2.Y) * (p1.Y - p2.Y));
